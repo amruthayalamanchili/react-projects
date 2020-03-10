d4e4d050712df481dd4cdb6a07594a806c29ef3a
amrutha <yalamanchili.amrutha@gmail.com>
2020-03-10 18:31:53 -0400

    added colors using chroma

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import seedPalettes from './seedPalettes';
 import Palette from './Palette';
-function App() {
-	return (
-		<div className="App">
-			<Palette {...seedPalettes[3]} />
-		</div>
-	);
+import { generatePalette } from './ColorHelpers';
+class App extends Component {
+	render() {
+		console.log(generatePalette(seedPalettes[4]));
+		return (
+			<div className="App">
+				<Palette {...seedPalettes[3]} />
+			</div>
+		);
+	}
 }
 
 export default App;

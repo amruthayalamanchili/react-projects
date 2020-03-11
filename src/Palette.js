059bcf2a0bac7ecf9e75c7234e84ceab9a454b15
amrutha <yalamanchili.amrutha@gmail.com>
2020-03-11 10:21:13 -0400

    added select and imported material-ui

--- src/Palette.js
+++ src/Palette.js
@@ -6,19 +6,23 @@ import './Palette.css';
 class Palette extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { level: 500 };
+		this.state = { level: 500, format: 'hex' };
 		this.changeLevel = this.changeLevel.bind(this);
+		this.changeFormat = this.changeFormat.bind(this);
 	}
 	changeLevel(level) {
 		this.setState({ level });
 	}
+	changeFormat(val) {
+		this.setState({ format: val });
+	}
 	render() {
 		const { colors } = this.props.palette;
-		const { level } = this.state;
-		const colorBox = colors[level].map((color) => <ColorBoxes background={color.hex} name={color.name} />);
+		const { level, format } = this.state;
+		const colorBox = colors[level].map((color) => <ColorBoxes background={color[format]} name={color.name} />);
 		return (
 			<div className="Palette">
-				<Navbar level={level} changeLevel={this.changeLevel} />
+				<Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
 				{/* navbar goes here */}
 				<div className="Palette-boxes">{colorBox}</div>
 				{/* footer goes here */}

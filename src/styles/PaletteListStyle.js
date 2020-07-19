8cbbad260c3feff71e609b1e79879eb73bfbb4ee
amrutha <yalamanchili.amrutha@gmail.com>
2020-07-19 06:54:53 -0400

    make paletteList responsive

--- src/styles/PaletteListStyle.js
+++ src/styles/PaletteListStyle.js
@@ -1,27 +1,38 @@
+import sizes from './sizes';
 export default {
 	root: {
-		backgroundColor: 'blue',
 		height: '100vh',
 		display: 'flex',
 		alignItems: 'flex-start',
 		justifyContent: 'center',
+		/* background by SVGBackgrounds.com */
+		backgroundColor: '#394bad',
+		// backgroundImage: `url(${bg})`,
 		overflow: 'scroll'
+	},
+	heading: {
+		fontSize: '2rem'
 	},
 	container: {
 		width: '50%',
 		display: 'flex',
 		alignItems: 'flex-start',
 		flexDirection: 'column',
-		flexWrap: 'wrap'
+		flexWrap: 'wrap',
+		[sizes.down('xl')]: {
+			width: '80%'
+		},
+		[sizes.down('xs')]: {
+			width: '75%'
+		}
 	},
 	nav: {
-		width: '100%',
-		color: 'white',
 		display: 'flex',
+		width: '100%',
 		justifyContent: 'space-between',
 		alignItems: 'center',
+		color: 'white',
 		'& a': {
-			fontWeight: '500',
 			color: 'white'
 		}
 	},
@@ -30,13 +41,13 @@ export default {
 		width: '100%',
 		display: 'grid',
 		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '2.5rem'
-		// [sizes.down("md")]: {
-		//   gridTemplateColumns: "repeat(2, 50%)"
-		// },
-		// [sizes.down("xs")]: {
-		//   gridTemplateColumns: "repeat(1, 100%)",
-		//   gridGap: "1.4rem"
-		// }
+		gridGap: '2.5rem',
+		[sizes.down('md')]: {
+			gridTemplateColumns: 'repeat(2, 50%)'
+		},
+		[sizes.down('xs')]: {
+			gridTemplateColumns: 'repeat(1, 100%)',
+			gridGap: '1.4rem'
+		}
 	}
 };
